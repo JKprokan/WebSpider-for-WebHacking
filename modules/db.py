8081ee097e551eccc5220a2c5d1ba7a62a0a6f3e
JKprokan <dlwlstjr0109@gmail.com>
2025-06-04 05:16:47 +0900

    input_fields에 form정보 추가

--- modules/db.py
+++ modules/db.py
@@ -24,11 +24,11 @@ def create_table():
 
 def insert_link(link, parent, depth, host, query_params, input_fields_json):
     kst_now = datetime.now(timezone(timedelta(hours=9))).strftime("%Y-%m-%d %H:%M:%S")
-
     with sqlite3.connect(db_path) as conn:
         cursor = conn.cursor()
         cursor.execute("""
-            INSERT OR IGNORE INTO crawl_links (link, parent, depth, host, query_params, input_fields, collected_time)
+            INSERT OR IGNORE INTO crawl_links 
+            (link, parent, depth, host, query_params, input_fields, collected_time)
             VALUES (?, ?, ?, ?, ?, ?, ?)
         """, (link, parent, depth, host, query_params, input_fields_json, kst_now))
         conn.commit()
