f55eaa2272c06d4c8ded2bc045642c4469a418d9
mercure <rhdmstnrk@gmail.com>
2025-07-12 17:42:29 +0900

    동적 크롤-컨텐츠 로드 완료후 수집으로 수정

--- modules/dynamic_crawler.py
+++ modules/dynamic_crawler.py
@@ -79,8 +79,8 @@ async def fetch_page(context, url, depth, parent, include_patterns, exclude_patt
 
     try:
         page = await context.new_page()
-        await page.goto(url, timeout=7000, wait_until="domcontentloaded")
-        await page.wait_for_load_state("domcontentloaded")
+        await page.goto(url, timeout=7000, wait_until="networkidle")
+        await page.wait_for_load_state("networkidle")
         content = await page.content()
         soup = BeautifulSoup(content, "html.parser") 
 
